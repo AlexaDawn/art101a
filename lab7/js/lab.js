ce0309f2feedc5066e6e1568ff89bc041962671a
AlexaDawn <adawnmarquez@gmail.com>
2023-05-03 22:02:28 -0700

    lab7stuff

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -9,15 +9,16 @@ function NameSorter() {
   var userName = window.prompt("Hi. Let me alphebatize your name!");
   console.log("userName =", userName);
   // split string to array
-  var nameArray = user.split('');
+  var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
   //sort array
   var nameArraySort = nameArray.sort();
   console.log("nameArray.sort=", nameArraySort);
   //join array back to a string
-  var nameSorted = nameArraySort.join('');
+  var nameSorted = nameArraySort.join();
   console.log("nameSorted =", nameSorted);
   return nameSorted;
 }
 
-document.writeln("Here's your alphabetized name:", NameSorter(), "</br>");
+document.writeln("Here's your alphabetized name: ", 
+  NameSorter(), "</br>");
