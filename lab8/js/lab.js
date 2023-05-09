bb84eeaeb21ff8c0c53b70a149efb013765d3e98
AlexaDawn <adawnmarquez@gmail.com>
2023-05-08 19:36:42 -0700

    lab8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,5 +1,5 @@
 // index.js - purpose and description here
-// Author: Your Name
+// Author: Alexa Marquez
 // Date:
 
 // Constants
@@ -7,15 +7,25 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+function square(x){
+  return (x ** 2);
+}
   // some code here
   // return results;
-}
+console.log("What is 21 squared?", square(21));
+console.log("What is 69 squared?", square(69));
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+numbers = [69, 7, 62, 2, 63, 42,]
+console.log("My numbers: ", numbers);
 
-// let's get this party started
-main();
+var result = numbers.map(square);
+console.log("My numbers squared: ", result);
+
+var result = numbers.map(function(x){
+  return x + x;
+})
+
+console.log("The double of each number", result);
+
+var outputEl = document.getElementById("output"); 
+outputEl.innerHTML = mapResults; // put your results here
