8083cf4a8c2ae662de4be4c6a1480f73c6014628
AlexaDawn <adawnmarquez@gmail.com>
2023-05-16 00:59:51 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,10 +1,28 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Alexa Marquez
+// Date: 15 May 2023
 
 // Constants
 
 // Functions
+
+function sortMe(str) {
+	return str.split('').sort().join('');
+}
+
+var buttonEl = document.getElementById("my-button");
+// Create an event handler within the JS file that listens for a button click.
+buttonEl.addEventListener("click", function(){
+	console.log("Button pressed");
+  // prompt the user to input their name with the prompt() function. 
+  //var name = prompt("What is your name?");
+  var name = document.getElementById("getit").value;
+  var nameStr = "Hello, " + sortMe(name);
+  // change the text from ‘Hello, World!’ to ‘Hello’ + the user’s name
+  var helloEl = document.getElementById("output");
+  helloEl.innerHTML = nameStr;
+})
+
 
 var button = document.createElement("button");
 button.innerHTML = "Do Something";
@@ -17,6 +35,3 @@ body.appendChild(button);
 button.addEventListener ("click", function() {
   alert("did something");
 });
-
-
-
