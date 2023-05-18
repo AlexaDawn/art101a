c75f0aaa3c5099c803c1744101b3d8c43976e427
AlexaDawn <adawnmarquez@gmail.com>
2023-05-18 01:38:06 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 11 - Jquery
+// Author: Alexa Marquez
+// Date: 17 May
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+$("#challenge").append("<button id='challenge-button'>Button</button>");
+console.log("Create Challenge Button");
+$("#problem").append("<button id='problem-button'>Button</button>");
+console.log("Create Problem Button");
+$("#results").append("<button id='results-button'>Button</button>");
+console.log("Create Results Button");
 
-// let's get this party started
-main();
+
+
+
+
+$("#challenge-button").click(function(){
+  $("#challenge").toggleClass("small");
+  console.log("Toggle Challenge");
+})
+
+$("#problem-button").click(function(){ 
+  $("#problem").toggleClass("new");
+  console.log("Toggle Problem");
+  $("#problem").css("color", "cyan");
+  console.log("Change Problem Color");
+})
+
+$("#results-button").click(function(){
+  $("#results").toggleClass("special");
+  console.log("Toggle Results");
+})
+  
