e35d51eeb359bf52ad4b40c12e49d816e65d9f4f
AlexaDawn <adawnmarquez@gmail.com>
2023-05-31 22:38:10 -0700

    lab15stuff

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -8,20 +8,32 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 
+//function getStuff() {
+//	console.log("Clicked!");
+//  $.ajax({
+//    url: "https://yerkee.com/api/fortune/cookie",
+//    type: "GET",
+//    data: {},
+//  })
+//  .done(function(data) {
+ 		// console.log(data);
+//    console.log(data);
+//    var answer = data.answer;
+//    $("#output").html("<h2>" + answer);
+//  })
+//}
+
 function getStuff() {
-	console.log("Clicked!");
+	console.log("Clicked");
   $.ajax({
-    url: "https://yerkee.com/api/cookie",
+    url: "https://www.boredapi.com/api/activity/",
     type: "GET",
     data: {},
   })
   .done(function(data) {
  		// console.log(data);
-    console.log(data);
-    var answer = data.answer;
-    var imgURL = data.image;
-    $("#output").html("<h2>" + answer);
-    $("#output").append("<img src=" + imgURL + ">");
+    console.log(data.activity);
+    $("#output").html("<h2>"+ data.activity);
   })
 }
 
